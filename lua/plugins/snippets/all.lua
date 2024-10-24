0a232d849e0ba6d503ba811c0240be4341fc9d5e
romai-nge <romainmalice@gmail.com>
2024-10-24 16:58:23 +0200

    Test snippet
    Changed snippets to just 1 test.
    Lazy-lock.json update for previous commit added.

--- lua/plugins/snippets/all.lua
+++ lua/plugins/snippets/all.lua
@@ -1,19 +1,10 @@
 local ls = require("luasnip")
+local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 
-return {
-	-- Example: how to set snippet parameters
-	require("luasnip").snippet(
-		{ -- Table 1: snippet parameters
-			trig = "hi",
-			dscr = "An autotriggering snippet that expands 'hi' into 'Hello, world!'",
-			regTrig = false,
-			priority = 100,
-			snippetType = "autosnippet",
-		},
-		{ -- Table 2: snippet nodes (don't worry about this for now---we'll cover nodes shortly)
-			t("Hello, world!"), -- A single text node
-		}
-		-- Table 3, the advanced snippet options, is left blank.
-	),
-}
+ls.add_snippets("lua", {
+    s("hello", {
+        t('print("hello world!")'),
+    }),
+})
