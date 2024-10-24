e708f52ca46866ab0f25acca11d4ffc793b75849
romai-nge <romainmalice@gmail.com>
2024-10-24 18:21:45 +0200

    Working autosnippets

--- lua/plugins/luasnippets/all.lua
+++ lua/plugins/luasnippets/all.lua
@@ -1,5 +1,8 @@
 ---@diagnostic disable: undefined-global
 
+-- 2 lists the first for normal snippets and the second for autosnippets
 return {
 	s("test", t("LOADED")),
+}, {
+	s("(", { t("("), i(1), t(")") }),
 }
