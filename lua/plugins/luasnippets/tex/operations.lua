a9eea7f3b64554841bde0b6d4ef7c022d8b51016
romai-nge <romainmalice@gmail.com>
2024-10-27 14:45:40 +0100

    Added obsidian latex suite text snippets (w/o insert node, just simple ones)

--- lua/plugins/luasnippets/tex/operations.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable: undefined-global
-
-local in_mathzone = function()
-	return vim.fn["vimtex#syntax#in_mathzone"]() == 1
-end
-
-return {}, {
-	s({trig="sr", wordTrig=false}, { t("^{2}") }, { condition = in_mathzone }),
-}
